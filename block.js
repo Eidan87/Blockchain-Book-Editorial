fc2467f3a8f26076bd5340236eb5d3fb36e479b8
Serhiy T.D <42222419+Eidan87@users.noreply.github.com>
2024-06-23 20:11:23 +0200

    Update block.js

--- block.js
+++ block.js
@@ -1,30 +1,36 @@
-const SHA256 = require('sha256')
+//block.js
+const SHA256 = require('sha256');
 
 class Block {
-    constructor(timestamp, transactions, tituloLibro, autorLibro, contenidoLibro, cantidad, hashPrevio=''){
-        this.timestamp = timestamp
-        this.transactions = transactions
-        this.tituloLibro = tituloLibro
-        this.autorLibro = autorLibro
-        this.contenidoLibro = contenidoLibro
-        this.cantidad = cantidad
-        this.hashPrevio = hashPrevio
-        this.comodin = 0
-        this.hash = this.calcularHash()
+    constructor(index, timestamp, transactions, tituloLibro, autorLibro, contenidoLibro, cantidad, hashPrevio = '', delegado = '', imageUrl = '') {
+        this.index = index;
+        this.timestamp = timestamp;
+        this.transactions = transactions;
+        this.tituloLibro = tituloLibro;
+        this.autorLibro = autorLibro;
+        this.contenidoLibro = contenidoLibro;
+        this.cantidad = cantidad;
+        this.hashPrevio = hashPrevio;
+        this.comodin = 0;
+        this.hash = this.calcularHash();
+        this.delegado = delegado;
+        this.imageUrl = imageUrl; // Nuevo campo para la URL de la imagen
     }
 
-calcularHash(){
-    return SHA256(this.timestamp + this.transactions + this.tituloLibro + this.autorLibro + JSON.stringify(this.contenidoLibro) + this.hashPrevio + this.cantidad + this.comodin).toString()
+    calcularHash() {
+        return SHA256(
+            this.index + this.timestamp + this.transactions +
+            this.tituloLibro + this.autorLibro + JSON.stringify(this.contenidoLibro) +
+            this.hashPrevio + this.cantidad + this.comodin + this.imageUrl // Incluye imageUrl en el hash
+        ).toString();
+    }
+
+    minarBloque(dificultad) {
+        while (this.hash.substring(0, dificultad) !== Array(dificultad + 1).join('0')) {
+            this.comodin++;
+            this.hash = this.calcularHash();
+        }
+    }
 }
 
-minarBloque(dificultad){
-    while(this.hash.substring(0, dificultad) !== Array(dificultad+1).join('0')){
-        this.comodin++
-        this.hash = this.calcularHash()
-    }  
-    //console.log('Bloque de transacciones o publicación publicado!!!: ' + this.hash)
-}
-
-}
-
-module.exports = Block
+module.exports = Block;
